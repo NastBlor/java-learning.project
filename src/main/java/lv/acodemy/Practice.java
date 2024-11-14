7ffd496f6ccdd20dd7e271b298ab65811a32dd25
Anastasija_Zvereva <NastBlor@gmail.com>
2024-11-14 21:13:22 +0200

    Practice

---
+++ src/main/java/lv/acodemy/Practice.java
@@ -0,0 +1,16 @@
+package lv.acodemy;
+
+public class Practice {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+        for(int i=1; i<=10; i++) {
+            System.out.println(i*5);
+        }
+    }
+}
